09ac42a7d03a5a1b49ae2f254989a199b9a53732
Joaquin <joaquin.zatarain@cceo.com.mx>
2021-12-15 14:32:29 -0600

    Se implementaron slots

---
+++ src/mostrarformulario.vue
@@ -0,0 +1,21 @@
+<template>
+    <form action="" class="form">
+        <slot name="titulo"></slot>
+        <slot name="elementos"></slot>
+        <slot name="boton"></slot>
+       
+    </form>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+  h3{
+    color: green;
+  }
+</style>
